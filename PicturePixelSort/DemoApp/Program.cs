f925f58b023a2b3d291ff387d62313a06f38178c
nedas-sim <nedas.sim@gmail.com>
2022-07-10 10:43:33 +0300

    Added array prep

--- PicturePixelSort/DemoApp/Program.cs
+++ PicturePixelSort/DemoApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using Models.Array;
+using System.Drawing;
 using System.Drawing.Imaging;
 
 namespace DemoApp;
@@ -9,6 +10,8 @@ public class Program
     readonly static string _outputDirectory = "..\\..\\..\\outputPhotos\\";
 
     readonly static string[] _directories = new[] { _inputDirectory, _outputDirectory };
+
+    const int _bmpMetadataOffset = 54;
 
     static void Main(string[] args)
     {
@@ -24,14 +27,48 @@ public class Program
         }
         Console.WriteLine($"Found file: '{fileName}'");
 
-        (int width, int height) = SaveBmpImage(fileName);
+        (int width, int height, string bmpFileName) = SaveBmpImage(fileName);
         Console.WriteLine($"Image width: {width}, height: {height}");
+
+        using (FileStream fs = new(bmpFileName, FileMode.Open, FileAccess.Read))
+        {
+            for (int sortingImageSize = 10; sortingImageSize <= Math.Min(width, height); sortingImageSize += 10)
+            {
+                fs.Seek(0, SeekOrigin.Begin);
+                ArrayRAM<byte> imageDataArray = new((int)fs.Length);
+                fs.Read(imageDataArray.GetArray(), 0, (int)fs.Length);
+
+                ArrayRAM<int> sortingArray = new(sortingImageSize * sortingImageSize);
+
+                int imageSeekPointer = _bmpMetadataOffset;
+                for (int i = 0; i < sortingImageSize; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (j < sortingImageSize)
+                        {
+                            fs.Seek(imageSeekPointer, SeekOrigin.Begin);
+                            byte blue = (byte)fs.ReadByte();
+                            byte green = (byte)fs.ReadByte();
+                            byte red = (byte)fs.ReadByte();
+                            int pixelValue = ((red << 8) + green << 8) + blue;
+                            sortingArray.Add(pixelValue);
+                        }
+                        imageSeekPointer += 3;
+                    }
+                }
+
+
+            }
+        }
+
+        Console.WriteLine("End of program");
     }
 
     static void TerminateProgram(string message)
     {
         Console.WriteLine(message);
-        Console.ReadKey();
+        Console.WriteLine("End of program");
     }
 
     #region void CreateDirectories()
@@ -80,18 +117,18 @@ public class Program
     }
     #endregion
 
-    #region (int Width, int Height) SaveBmpImage(string fileName)
+    #region (int Width, int Height, string SavedBmpFileName) SaveBmpImage(string fileName)
     /// <summary>
     /// Saves jpg image as bmp in the output directory.
     /// </summary>
     /// <param name="fileName">Jpg file name</param>
-    /// <returns>Image's width and height in a tuple</returns>
-    static (int Width, int Height) SaveBmpImage(string fileName)
+    /// <returns>Image's width, height and file name in a tuple</returns>
+    static (int Width, int Height, string SavedBmpFileName) SaveBmpImage(string fileName)
     {
         Bitmap image = new(fileName);
         string saveImagePath = Path.Combine(_outputDirectory, Path.GetFileNameWithoutExtension(fileName)) + ".bmp";
         image.Save(saveImagePath, ImageFormat.Bmp);
-        return (image.Width, image.Height);
+        return (image.Width, image.Height, saveImagePath);
     }
     #endregion
 }
