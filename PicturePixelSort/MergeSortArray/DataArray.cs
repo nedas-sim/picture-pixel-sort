6ce641330473f05b95e0a67afd54cd38424cee3b
nedas-sim <nedas.sim@gmail.com>
2025-11-22 22:35:28 +0200

    Remake in memory array sorting

---
+++ PicturePixelSort/MergeSortArray/DataArray.cs
@@ -0,0 +1,55 @@
+﻿namespace MergeSortArray;
+
+internal class DataArray<T> where T : IComparable
+{
+    private T[] Array;
+    private int Count;
+
+    public DataArray(int size)
+    {
+        Array = new T[size];
+        Count = 0;
+    }
+
+    public void Add(T data)
+    {
+        if (Count < Size())
+        {
+            Array[Count] = data;
+            Count++;
+        }
+    }
+
+    public void Replace(int index, T data)
+    {
+        Array[index] = data;
+    }
+
+    public T this[int index]
+    {
+        get { return Array[index]; }
+    }
+
+    public void Swap(int index1, int index2)
+    {
+        T temp = Array[index1];
+        Array[index1] = Array[index2];
+        Array[index2] = temp;
+    }
+
+    public T[] GetArray()
+    {
+        return Array;
+    }
+
+    public void SetArray(T[] arr)
+    {
+        Array = arr;
+    }
+
+    public int Size()
+    {
+        return Array.Length;
+    }
+}
+
---
+++ PicturePixelSort/readme.md
@@ -0,0 +1,15 @@
+# Picture Pixel Sort
+
+## Comments
+
+- Directories for input/output images need to set up manually
+- Unclear csv file structure
+- Unclear console logs
+- .NET Framework 4.6.1 -> .NET 10
+- Running on better hardware compared to when the project was first made
+
+# Algorithms
+
+## Merge sort (in-memory, array)
+
+- Same code runs 5 times faster
